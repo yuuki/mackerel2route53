21fa4db1ef8b9879ccd476d2186cb9f687e816b3
y_uuki <yuki.tsubo@gmail.com>
2018-02-04 20:29:04 +0900

    Combine with API Gateway

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func hello() (string, error) {
-	return "Hello ƛ!", nil
+func mackerelWebHookHandler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
+
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("    %s: %s\n", key, value)
+	}
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(hello)
+	lambda.Start(mackerelWebHookHandler)
 }
